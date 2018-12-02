9083472d595eda366517491f680398a8c22c94e2
Brandon Stinson <iamthequestion@gmail.com>
2018-12-02 04:33:16 -0800

    advent of code days 1 and 2

--- src/components/adventOfCode/AdventOfCode.js
+++ src/components/adventOfCode/AdventOfCode.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import Title from '../Title';
+import DayCard from './DayCard';
+import solutions from '../../data/adventOfCode/solutions';
+
 const StyledAdventOfCode = styled.div`
     display: grid;
+    grid-gap: 20px;
+    justify-content: center;
+    background-color: ${props => props.theme.offWhite};
+    color: ${props => props.theme.black};
+    padding: 40px;
 `;
 
 const AdventOfCode = () => (
     <StyledAdventOfCode>
-        <h1>Stuff goes here</h1>
+        <Title title="Advent of Code 2018" />
+        {solutions.map((solution, index) => (
+            <DayCard key={index} solution={solution} />
+        ))}
     </StyledAdventOfCode>
 );
 
--- src/pages/projects/advent.js
+++ src/pages/projects/advent.js
@@ -2,13 +2,11 @@ import React from 'react';
 
 import Layout from '../../components/Layout';
 import Header from '../../components/Header';
-import Title from '../../components/Title';
 import AdventOfCode from '../../components/adventOfCode/AdventOfCode';
 
 const AdventOfCodePage = () => (
     <Layout>
         <Header />
-        <Title title="Advent of Code 2018" />
         <AdventOfCode />
     </Layout>
 );
