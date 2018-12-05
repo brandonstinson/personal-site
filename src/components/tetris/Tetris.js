464fce42c2ac9638a21de51672dd357a6e9c5a2b
Brandon Stinson <iamthequestion@gmail.com>
2018-12-05 11:01:34 -0800

    hammer check

--- src/components/tetris/Tetris.js
+++ src/components/tetris/Tetris.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import Hammer from 'hammerjs';
+import { Hammer } from 'hammerjs';
 
 import Canvas from './Canvas';
 import ScoreCard from './ScoreCard';
