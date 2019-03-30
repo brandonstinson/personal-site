4161298e6266062eeb34c61872d4bdd8194f3956
Brandon Stinson <iamthequestion@gmail.com>
2019-03-30 05:44:51 -0500

    finished resources page

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -4,21 +4,67 @@ import styled from 'styled-components';
 import Layout from '../components/layout';
 import Header from '../components/header';
 import Title from '../components/title';
+import ContentContainer from '../components/contentContainer';
+
+import resources from '../utils/data/resources';
 
 const StyledResources = styled.div`
   display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(280px, 500px));
+  grid-gap: 2rem;
   justify-content: center;
   text-align: center;
-  padding: 40px;
+  .full-width {
+    grid-column: 1 / -1;
+  }
+  .sub-title {
+    font-size: 1.5rem;
+  }
+  .resource {
+    display: grid;
+    grid-template-rows: auto 1fr;
+    justify-items: center;
+    border-bottom: 3px solid ${props => props.theme.black};
+  }
+  }
+  .name {
+    background-color: ${props => props.theme.yellow};
+    padding: 0.5rem;
+    font-weight: 900;
+    font-size: 1.5rem;
+    transform: rotate(-5deg);
+    margin-bottom: 1rem;
+  }
+  .description {
+    font-size: 1rem;
+    margin-bottom: 1rem;
+  }
+  a {
+    color: inherit;
+    font-size: inherit;
+    text-decoration: none;
+  }
 `;
 
 const ResourcesPage = () => (
   <Layout>
     <Header />
     <Title title="Resources" />
-    <StyledResources>
-      <h3>Coming Soon...</h3>
-    </StyledResources>
+    <ContentContainer>
+      <StyledResources>
+        <div className="sub-title full-width">
+          These resources really helped me learn and grow as a developer
+        </div>
+        {resources.map((resource, index) => (
+          <div className="resource" key={index}>
+            <div className="name">
+              {resource.link ? <a href={resource.link}>{resource.name}</a> : resource.name}
+            </div>
+            <div className="description">{resource.description}</div>
+          </div>
+        ))}
+      </StyledResources>
+    </ContentContainer>
   </Layout>
 );
 
---
+++ src/utils/data/resources.js
@@ -0,0 +1,58 @@
+const resources = [
+  {
+    name: `freeCodeCamp`,
+    link: `https://freecodecamp.org`,
+    description: `An incredible resource for learning to code. The free curriculum covers topics like HTML, CSS, JavaScript, 
+      data structures, algorithms, frontend libraries, data visualization, APIs, and more.`,
+  },
+  {
+    name: `Wes Bos Courses`,
+    link: `https://wesbos.com/courses/`,
+    description: `Wes Bos is a full stack web developer with free and paid courses cover topics like React, CSS Grid, 
+      ES6, NodeJS, and Redux.`,
+  },
+  {
+    name: `dev.to`,
+    link: `https://dev.to/`,
+    description: `Not just a site, dev.to is an inclusive community with articles on a variety of topics.`,
+  },
+  {
+    name: `CSS Tricks`,
+    link: `https://css-tricks.com/`,
+    description: `Great articles on everything CSS. Their CSS Grid and Flexbox guides are amazing.`,
+  },
+  {
+    name: `Net Ninja YouTube Channel`,
+    link: `https://www.youtube.com/channel/UCW5YeuERMmlnqo4oq8vwUpg`,
+    description: `The Net Ninja has YouTube video tutorials covering topics like React, Web Sockets, Vue, Git & Github, 
+      MongoDB, SASS, Python, NodeJS, and Regular Expressions.`,
+  },
+  {
+    name: `CodeNewbie`,
+    link: `https://www.codenewbie.org/`,
+    description: `CodeNewbie is a community focused on newer developers. The have a two weekly Twitter chats, a podcast, 
+      and an annual conference.`,
+  },
+  {
+    name: `Podcasts`,
+    description: `I listen to the following podcasts: Syntax, React Podcast, Frontend Happy Hour, The Diff, JS Party, 
+      Toolsday, and CodePen Radio.`,
+  },
+  {
+    name: `freeCodeCamp on Medium`,
+    link: `https://medium.freecodecamp.org/`,
+    description: `Countless useful posts on every subject related to development.`,
+  },
+  {
+    name: `Traversy Media YouTube Channel`,
+    link: `https://www.youtube.com/channel/UC29ju8bIPH5as8OGnQzwJyA`,
+    description: `Another YouTuber with a series of Crash Courses (1-2 hour videos on various subjects) and tutorials.`,
+  },
+  {
+    name: `MDN Web Docs`,
+    link: `https://developer.mozilla.org/en-US/`,
+    description: `The MDN web docs are always my first stop when learning something new.`,
+  },
+];
+
+export default resources;
